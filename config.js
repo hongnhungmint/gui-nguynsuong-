7ebc797c80eb2ff1fc804d35a4f10ff7a7ec9857
hongnhungmint <86786792+hongnhungmint@users.noreply.github.com>
2021-07-01 20:49:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'Hé lu Nguỹn Sương!',
+    introDesc: `Hôm nay emiu của t thế nào? Có mệt hơm!?`,
+    btnIntro: 'Chạm nhẹ vào đây nè!',
+    title: 'Nếu t đoán khum nhầm thì... 😙',
+    desc: 'Phải chăng m đang nhớ t đúng hummmm ',
+    btnYes: 'Gấc nhớ luôngg <33',
+    btnNo: 'Không :3',
+    question:'Có lời nào muốn nhắn nhủ đến một người cute như t humm <3',
+    btnReply: 'Gửi cho Bơ <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Yêu gấc nhiều 😘😘',
+    messDesc: 'Ôm mụt cái',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/Ektdan.Kate' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
